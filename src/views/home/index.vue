80e00db6609fc296eb7bbfd22cd981adaa00e9c6
unknown <2304248987@qq.com>
2022-03-05 22:52:01 +0800

    轮播图-结构渲染

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="container">
-    <!-- 首页入口（分类+广告） -->
-    <div class="home-entry">
+  <!-- 首页入口（分类+广告） -->
+  <div class="home-entry">
+    <div class="container">
       <!-- 左侧分类 -->
       <HomeCategory />
       <!-- 轮播图 -->
+      <HomeBanner />
     </div>
   </div>
 </template>
 
 <script>
 import HomeCategory from './components/home-category.vue'
+import HomeBanner from './components/home-banner.vue'
 export default {
   name: 'HomePage',
   components: {
-    HomeCategory
+    HomeCategory,
+    HomeBanner
   }
 }
 </script>
